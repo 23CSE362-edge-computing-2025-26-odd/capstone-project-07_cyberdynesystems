089bd4a3b073b16fa93af5c0bc612498f278045d
YuvanDhurkesh <yuvansj2005@gmail.com>
2025-09-14 22:32:53 +0530

    Integrated neural network from CI

--- review2/client.py
+++ review2/client.py
@@ -1,4 +1,4 @@
-'''
+"""
 Preamble:
 class Client(): The class that simulates an actual edge device.
     self.ID: (int) Assigned uniquely. Used to distinguish between clients.
@@ -9,38 +9,41 @@ class Client(): The class that simulates an actual edge device.
         self.CLIENT_RESOURCES["IDLE"]: Simulates the IDLE state of the client
         self.CLIENT_RESOURCES["PLUGGED"]: Simulates the PLUGGED state of the client
     self.TRAINING: (bool) Used to check if the client is chosen for training or inferencing
-    self.NET: Used to set up the neural network in the client-end
+    self.NET: (MNIST.SimpleNN()) Used to set up the neural network in the client-end
     self.LOCAL_DATASET_SIZE: (int) The size of the local dataset on the client
-    
+    self.LOCAL_DATASET: (np.array(uint8)) This is the local dataset stored in the client object
+
     local_training(epochs: number of epochs to train):
         updates to model parameters and accuracy/loss: Trains the received model by applying SGD/Adam and sends the updates
 
     inference(): forward pass the input and return the output
 
-    threshold_calc(): Computes the *score based on the client parameters
+    compatibility_checker(): returns a boolean based on the resource parameters of the client
 
     recv_parameters(params_dict: weights and bias of the global model): Set the global model parameters to the local model
 
     (FINAL) comm_interface():
     Uses TCP socket to connect to the server. Sends the local model updates, parameters of the client. Receives the global model parameters.
+"""
+import MNIST
 
-'''
+class Client:
+    def __init__(self, client_id: int, cpu_usage: int, ram_usage: int, charge: int, idle: bool, plugged: bool, local_dataset):
+        self.ID = client_id
+        self.CLIENT_RESOURCES =  {"CPU": cpu_usage, "RAM": ram_usage, "CHARGE": charge, "IDLE": idle, "PLUGGED": plugged}
+        self.TRAINING = False
+        self.NET = MNIST.SimpleNN()
+        self.LOCAL_DATASET_SIZE = len(local_dataset)
+        self.LOCAL_DATASET = local_dataset
 
-class Client():
-    def _init_(self, cpu_usage: int, total_cores: int, ram_usage: int, total_ram: int, charge: int, idle: bool, plugged: bool):
-        self.ID = None
-        self.CLIENT_RESOURCES =  {"CORESUSAGE": 0, "TOTALCORES": 0, "RAM": 0, "TOTALRAM": 0, "CHARGE": 0, "IDLE": None, "PLUGGED": None}
-        self.TRAINING = None
-        self.NET = None
-        self.LOCAL_DATASET_SIZE = 0
-    
-    def local_training(self, epochs):
-	pass
+    # Function to check the compatibility of the client with the server process
+    def compatibility_check(self) -> bool:
+        pass
+
+    def local_training(self, pso_epochs):
+        pass
 
     def inference(self, x):
-	pass
-
-    def threshold_calc(self):
         pass
 
     def recv_parameters(self, params_dict):
